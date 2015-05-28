6f4f2adf4c8d4c033536d97f409dc4660bf314fb
dhacker29 <dhackerdvm@gmail.com>
2015-05-28 00:13:14 -0400

    proprietary-files and props: Update for 5.1.1
    Change-Id: I8db045bbc80fba70950b665640761bb4a2d736cb
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -240,6 +240,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.ignore_ims_wlan=1 \
     persist.radio.data_con_rprt=1
 
+# Rich Communications Service is disabled in 5.1
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
+    persist.radio.ignore_ims_wlan=1 \
+    persist.radio.data_con_rprt=1
+
 #Reduce IMS logging
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.ims.disableDebugLogs=1
