e957c1068ac37aeddb571899833093bb7bdbb5a9
TeamFahQ <teamfahq1@gmail.com>
2015-07-21 06:10:15 -0500

    swapping the deminsions around

--- broken.mk
+++ broken.mk
@@ -1,6 +1,6 @@
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1440
-TARGET_SCREEN_WIDTH := 2560
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
 
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/broken/config/gsm.mk)
