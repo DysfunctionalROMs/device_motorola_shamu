78a5519fd72daaa8d789d4a03dbe39f769c5669d
TeamFahQ <teamfahq1@gmail.com>
2015-09-04 22:46:17 -0500

    Remove duplicate

--- device.mk
+++ device.mk
@@ -272,7 +272,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.force_eri_from_xml=true \
     ro.telephony.default_network=3 \
-    ro.telephony.default_network=10 \
     telephony.lteOnCdmaDevice=1
 
 # SIM based FSG loading & MCFG activation
