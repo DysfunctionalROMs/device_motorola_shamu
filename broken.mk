05aa6b7d6668b6fec209db02e78bc7916f846801
TeamFahQ <teamfahq1@gmail.com>
2015-12-31 23:41:08 -0600

    Add device maintainer

--- broken.mk
+++ broken.mk
@@ -28,3 +28,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
     BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MMB29K/2419427:user/release-keys \
     PRIVATE_BUILD_DESC="shamu-user 6.0.1 MMB29K 2419427 release-keys"
+
+# Broken Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Thomas.Raines"
