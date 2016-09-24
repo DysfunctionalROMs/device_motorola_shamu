2bfaad386a4df36e5fc9e0e2aa25457ad1772ec7
roguesyko <roguesyko@gmail.com>
2016-09-24 18:43:34 -0400

    Add telephony-ext package and boot jar Taken from broken/bacon

--- device.mk
+++ device.mk
@@ -446,3 +446,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # OEM Unlock reporting
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.oem_unlock_supported=1
+
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
