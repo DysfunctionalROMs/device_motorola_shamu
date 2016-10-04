e6fec5f399f87abf83a73d1726ef35f9475f69c3
TeamFahQ <teamfahq1@gmail.com>
2016-10-04 05:42:13 -0500

    update BUILD_FINGERPRINT:NBD90Z

--- broken.mk
+++ broken.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := Nexus 6
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.0.0/NRD90M/3085278:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 7.0.0 NRD90M 3085278 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:7.0.0/NBD90Z/3264873:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.0.0 NBD90Z 3264873 release-keys"
 
 # Broken Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
