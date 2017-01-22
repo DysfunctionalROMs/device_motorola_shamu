f609210031f221f6d5f524f73514f433e5081443
TeamFahQ <teamfahq1@gmail.com>
2017-01-22 06:39:35 -0600

    Initial setup before rebase
    Change-Id: Id46a52da471826d40e92d90caaf897504eaf8580

---
+++ cm.mk
@@ -0,0 +1,31 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+## Specify phone tech before including full_phone Depricated?
+## $(call inherit-product, vendor/broken/config/gsm.mk)
+
+# Inherit some common broken stuff.
+$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
+
+# Enhanced NFC
+# $(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/shamu/aosp_shamu.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/motorola/shamu/overlay-broken
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := bs_shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:7.0.0/NBD90Z/3264873:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.0.0 NBD90Z 3264873 release-keys"
+
+# Broken Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Thomas Raines"
